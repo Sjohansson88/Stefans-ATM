0451ed488550444612b14a03f9139334c7bf170a
Stefan J <sjohansson88@hotmail.com>
2023-10-05 21:13:10 +0200

    antal inloggningförsök

--- Program.cs
+++ Program.cs
@@ -2,8 +2,18 @@
 {
     internal class Program
     {
+
         static string[] kund = { "Stefan", "Tobias", "Anas", "Johanna", "Pär" };
         static string[] pinkod = { "1111", "2222", "3333", "4444", "5555" };
+        static string[][] kontonArray =
+            {
+            new string[] { "Sparkonto", "Lönekonto" },
+            new string[] { "Sparkonto" },
+            new string[] {"Lönekonto", "Resekonto" },
+            new string[] { "Bilkonto", "Lönekonto" },
+            new string[] { "Fondsparande" }
+            };
+
 
         static bool LoggaIn(string användarnamn, string lösenord)
         {
@@ -11,6 +21,7 @@
             {
                 if (användarnamn == kund[i] && lösenord == pinkod[i])
                 {
+                    Console.WriteLine("Välkommen " + kund[i]);
                     return true;
                 }
             }
@@ -19,23 +30,73 @@
 
         static void Main(string[] args)
         {
-            Console.WriteLine("--------------------Välkommen till Stefans ATM----------------------");
-            Console.Write("Ange kundnamn:\n ");
-            string användarnamn = Console.ReadLine();
-            Console.Write("Ange pinkod:\n ");
-            string lösenord = Console.ReadLine();
+            int försök = 0;
+            int maxFörsök = 3;
+            bool lyckadInloggning = false;
 
-            if (LoggaIn(användarnamn, lösenord))
+            while (försök < maxFörsök && !lyckadInloggning)
             {
-                Console.WriteLine("Kundnamn korrekt");
-                Console.Clear();
+                Console.WriteLine("--------------------Välkommen till Stefans ATM----------------------");
+                Console.Write("Ange kundnamn:\n ");
+                string användarnamn = Console.ReadLine();
+                Console.Write("Ange pinkod:\n ");
+                string lösenord = Console.ReadLine();
 
+                if (LoggaIn(användarnamn, lösenord))
+                {
+                    Console.WriteLine("Kundnamn korrekt");
+                    Console.Clear();
+                    lyckadInloggning = true;
+                }
+                else
+                {
+                    Console.WriteLine("Fel kundnamn eller pinkod. Försök igen.");
+                    försök++;
+                }
+            }
+
+            if (!lyckadInloggning)
+            {
+                Console.WriteLine("Du har överträtt maximalt antal inloggningsförsök. Programmet avslutas.");
             }
             else
             {
-                Console.WriteLine("Fel kundnamn eller pinkod. Försök igen.");
-            }
+                int choice = 0;
+                while (true)
+                {
+                    Console.WriteLine("--------------------Välkommen till Stefans ATM----------------------\n");
+                    Console.WriteLine("1. Se konton och saldo\n");
+                    Console.WriteLine("2. Överföring mellan konton\n");
+                    Console.WriteLine("3. Ta ut pengar\n");
+                    Console.WriteLine("4. Logga ut\n");
 
+                    Console.WriteLine("Ange val");
+                    choice = Convert.ToInt32(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Konton och saldo: ");
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Från vilket konto vill du föra över pengar från");
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Ange belopp för uttag: ");
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Tack för idag!");
+                            break;
+
+                        default:
+                            Console.WriteLine("Felaktigt val!");
+                            break;
+                    }
+                }
+            }
         }
     }
 }

4377fb057a722738581b6b899d42d5f9d0bfe3f1
Stefan J <sjohansson88@hotmail.com>
2023-10-10 21:06:25 +0200

    Konton

--- Program.cs
+++ Program.cs
@@ -1,25 +1,40 @@
-﻿namespace Stefans_ATM
+﻿using System;
+
+namespace Stefans_ATM
 {
     internal class Program
     {
 
-        static string[] kund = { "Stefan", "Tobias", "Anas", "Johanna", "Pär" };
+        static string[] kund = { "stefan", "tobias", "anas", "johanna", "pär" };
         static string[] pinkod = { "1111", "2222", "3333", "4444", "5555" };
-        static string[][] kontonArray =
-            {
-            new string[] { "Sparkonto", "Lönekonto" },
-            new string[] { "Sparkonto" },
-            new string[] {"Lönekonto", "Resekonto" },
-            new string[] { "Bilkonto", "Lönekonto" },
-            new string[] { "Fondsparande" }
-            };
 
+      
+        static string[][] användaresKonton = new string[5][];
+        static decimal[][] användaresSaldon = new decimal[5][];
 
-        static bool LoggaIn(string användarnamn, string lösenord)
+       
+        static void SkapaAnvändarnasKonton()
+        {
+            användaresKonton[0] = new string[] { "Sparkonto", "Lönekonto" };
+            användaresSaldon[0] = new decimal[] { 1000.0m, 500.0m };  
+
+            användaresKonton[1] = new string[] { "Bilkonto" };           
+            användaresSaldon[1] = new decimal[] { 1500.0m };          
+
+            användaresKonton[2] = new string[] { "Båtkonto", "Sparkonto" }; 
+            användaresSaldon[2] = new decimal[] { 2000.0m, 3000.0m }; 
+
+            användaresKonton[3] = new string[] { "Aktiekonto", "Fondkonto" }; 
+            användaresSaldon[3] = new decimal[] { 750.0m, 300.0m };   
+
+            användaresKonton[4] = new string[] { "Matkonto" };          
+            användaresSaldon[4] = new decimal[] { 2500.0m };         
+        }
+        static bool LoggaIn(string userName, string password)
         {
             for (int i = 0; i < kund.Length; i++)
             {
-                if (användarnamn == kund[i] && lösenord == pinkod[i])
+                if (userName == kund[i] && password == pinkod[i])
                 {
                     Console.WriteLine("Välkommen " + kund[i]);
                     return true;
@@ -28,25 +43,30 @@
             return false;
         }
 
+
         static void Main(string[] args)
         {
+            bool användarenÄrInloggad = false;
+            string inloggadAnvändare = "";
+
             int försök = 0;
             int maxFörsök = 3;
-            bool lyckadInloggning = false;
 
-            while (försök < maxFörsök && !lyckadInloggning)
+            while (!användarenÄrInloggad && försök < maxFörsök)
             {
                 Console.WriteLine("--------------------Välkommen till Stefans ATM----------------------");
                 Console.Write("Ange kundnamn:\n ");
-                string användarnamn = Console.ReadLine();
+                string användarnamn = Console.ReadLine().ToLower();
                 Console.Write("Ange pinkod:\n ");
                 string lösenord = Console.ReadLine();
 
                 if (LoggaIn(användarnamn, lösenord))
                 {
-                    Console.WriteLine("Kundnamn korrekt");
+                    Console.WriteLine("Välkommen " + användarnamn);
                     Console.Clear();
-                    lyckadInloggning = true;
+
+                    inloggadAnvändare = användarnamn; 
+                    användarenÄrInloggad = true;
                 }
                 else
                 {
@@ -55,40 +75,78 @@
                 }
             }
 
-            if (!lyckadInloggning)
+            if (!användarenÄrInloggad)
             {
                 Console.WriteLine("Du har överträtt maximalt antal inloggningsförsök. Programmet avslutas.");
             }
             else
             {
-                int choice = 0;
-                while (true)
+                bool myBool = true;
+                while (myBool)
                 {
-                    Console.WriteLine("--------------------Välkommen till Stefans ATM----------------------\n");
-                    Console.WriteLine("1. Se konton och saldo\n");
-                    Console.WriteLine("2. Överföring mellan konton\n");
-                    Console.WriteLine("3. Ta ut pengar\n");
-                    Console.WriteLine("4. Logga ut\n");
+                    Console.WriteLine("[1]. Se konton och saldo\n");
+                    Console.WriteLine("[2]. Överföring mellan konton\n");
+                    Console.WriteLine("[3]. Ta ut pengar\n");
+                    Console.WriteLine("[4]. Logga ut\n");
 
                     Console.WriteLine("Ange val");
-                    choice = Convert.ToInt32(Console.ReadLine());
 
-                    switch (choice)
+                    int menyVal = 0;
+                    try
+                    {
+                        menyVal = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("\tFelaktigt kommando");
+                    }
+
+                    switch (menyVal)
                     {
                         case 1:
-                            Console.WriteLine("Konton och saldo: ");
+                            Console.Clear();
+
+                            SkapaAnvändarnasKonton();
+
+                            int användarIndex = Array.IndexOf(kund, inloggadAnvändare);
+
+                            if (användarIndex != -1)
+                            {
+                                for (int i = 0; i < användaresKonton[användarIndex].Length; i++)
+                                {
+                                    Console.WriteLine($"{användaresKonton[användarIndex][i]}, Saldo: {användaresSaldon[användarIndex][i]:C}");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Användaren hittades inte eller har inga konton.");
+                            }
+
+                            Console.WriteLine("Tryck Enter för att återgå till menyn...");
+                            Console.ReadLine();
+                            Console.Clear();
                             break;
 
                         case 2:
+                            Console.Clear();
                             Console.WriteLine("Från vilket konto vill du föra över pengar från");
+                            Console.WriteLine("Tryck Enter för att återgå till menyn...");
+                            Console.ReadLine();
+                            Console.Clear();
                             break;
 
                         case 3:
+                            Console.Clear();
                             Console.WriteLine("Ange belopp för uttag: ");
+                            Console.WriteLine("Tryck Enter för att återgå till menyn...");
+                            Console.ReadLine();
+                            Console.Clear();
                             break;
 
                         case 4:
-                            Console.WriteLine("Tack för idag!");
+                            Console.Clear();
+                            Console.WriteLine("Du har loggats ut.");
+                            myBool = false;
                             break;
 
                         default:
